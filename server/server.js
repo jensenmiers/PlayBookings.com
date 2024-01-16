86fdc4833aa44043cf30a09800041d54a07785e8
jensenmiers <jensenmiers@gmail.com>
2024-01-15 21:49:36 -0800

    template

--- server/server.js
+++ server/server.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('MEAN Stack Running'));
 
-app.listen(port, () => console.log('Server running on port ${port}'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
