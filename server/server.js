ee6449e3f298c694c927e41f34189115ba3d64df
jensenmiers <jensenmiers@gmail.com>
2024-01-17 20:54:32 -0800

    Location model added to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const Location = require('./models/Location');
+
 app.get('/', (req, res) => res.send('MEAN Stack Running'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
