938bf547d2b24cfbb1a05d37c89bc2a43d12c9cf
jensenmiers <jensenmiers@gmail.com>
2024-10-01 21:37:18 -0700

    new copy today

--- play-bookings/src/app/page.js
+++ play-bookings/src/app/page.js
@@ -18,8 +18,8 @@ export default function Home() {
         {/* Hero Section */}
         <section className="flex flex-col items-center justify-center h-screen bg-blue-600 text-white">
           <h2 className="text-4xl font-bold">Welcome to Play Bookings</h2>
-          <p className="mt-4 text-lg">Rent indoor gyms & outdoor fields or list your facilitites easily.</p>
-          <p className="mt-4 text-lg">Available courts & fields at your convenience.</p>
+          <p className="mt-4 text-lg">Rent available indoor gyms & outdoor fields at your convenience.</p>
+          <p className="mt-4 text-lg">Facility owners, list your locations easily</p>
           <button className="mt-6 px-6 py-2 bg-yellow-500 text-black font-semibold rounded">Sign up for updates and early access</button>
         </section>
 
@@ -28,15 +28,15 @@ export default function Home() {
           <h3 className="text-2xl font-bold text-center">Features</h3>
           <div className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-8">
             <div className="p-4 bg-gray-200 rounded">
-              <h4 className="font-bold">Easy booking</h4>
-              <p>Seamless booking experience for gym & facility rentals.</p>
+              <h4 className="font-bold">Easy bookings.</h4>
+              <p>Seamless booking experience for gym & field rentals.</p>
             </div>
             <div className="p-4 bg-gray-200 rounded">
-              <h4 className="font-bold">Rent unutilized spaces.</h4>
-              <p>Competitive pricing to reserve or list your facilities.</p>
+              <h4 className="font-bold">Fill un-used spaces.</h4>
+              <p>A competitive marketplace for both renters and facility owners.</p>
             </div>
             <div className="p-4 bg-gray-200 rounded">
-              <h4 className="font-bold">Verified locations and secure payments</h4>
+              <h4 className="font-bold">Verified locations.</h4>
               <p>Safe and reliable onboarding of venues, ensuring trust across our marketplace.</p>
             </div>
           </div>
