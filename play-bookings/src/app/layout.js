9ec636b62053fa7d1ccf6df016a15854f4fe0409
jensenmiers <jensenmiers@gmail.com>
2024-10-08 11:55:15 -0700

    new title

--- play-bookings/src/app/layout.js
+++ play-bookings/src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Play Bookings",
-  description: "Play Bookings",
+  title: "PlayBookings.com",
+  description: "PlayBookings.com",
 };
 
 export default function RootLayout({ children }) {
