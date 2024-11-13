65c8d5f74a4715f209019e6998f1d62e270d45ca
jensenmiers <jensenmiers@gmail.com>
2024-11-12 21:32:36 -0800

    viewport to 80vh

--- play-bookings/src/app/page.js
+++ play-bookings/src/app/page.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-[80vh] flex-col items-center justify-between p-24">
       <div>
         {/* Header */}
         <header className="flex justify-between p-4 bg-gray-800 text-white">
