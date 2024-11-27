40780989ed0c103d0fb5c75df0be4d58b99e3d68
jensenmiers <jensenmiers@gmail.com>
2024-11-27 07:53:21 -0800

    different mailchimp approach

--- play-bookings/src/app/api/subscribe.js
+++ play-bookings/src/app/api/subscribe.js
@@ -5,7 +5,7 @@ export async function POST(request) {
         const { email } = await request.json();
 
         // MAILCHIMP API URL & API KEY!!
-        const mailchimpUrl = `https://<dc>.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST_ID}/members`;
+        // const mailchimpUrl = `https://<dc>.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST_ID}/members`;
         const apiKey = process.env.MAILCHIMP_API_KEY;
 
         const response = await fetch(mailchimpUrl, {
