87d3d4e69b6a2895b6e40aff0773aa25eb25c3b5
jensenmiers <jensenmiers@gmail.com>
2024-12-03 07:47:27 -0800

    env.local file added

--- play-bookings/utils/supabaseClient.js
+++ play-bookings/utils/supabaseClient.js
@@ -1,2 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
+export const createClient = () => 
+    createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    );
