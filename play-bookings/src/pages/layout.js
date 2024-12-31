8f61b3df4e34b78ce1cc32532fdf9cecfe1b418e
jensenmiers <jensenmiers@gmail.com>
2024-12-31 13:42:13 -0800

    2025 all rights reserved

--- play-bookings/src/pages/layout.js
+++ play-bookings/src/pages/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
     </header>
     <main>{children}</main>
     <footer className="p-4 bg-gray-800 text-white text-center">
-      <p>&copy; 2024 Play Bookings LLC. All rights reserved.</p>
+      <p>&copy; 2025 Play Bookings LLC. All rights reserved.</p>
     </footer>
   </div>
   );
